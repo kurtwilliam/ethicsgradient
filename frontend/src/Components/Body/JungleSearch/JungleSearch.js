9ec53ede9b2edf8541a9aac427fdbbd3f7b70f6e
Kurt Schneider <k.w.schneid@gmail.com>
2018-12-10 18:05:49 -0500

    Deleting wrong package.json 🤷‍♂️ adding in proper locationg

--- frontend/src/Components/Body/JungleSearch/JungleSearch.js
+++ frontend/src/Components/Body/JungleSearch/JungleSearch.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import Chunk from "../../shared/Chunk";
+import Draggable from "react-draggable";
 
 class JungleSearch extends Component {
   render() {
-    return <Chunk />;
+    return (
+      <Chunk>
+        <Draggable
+          axis="x"
+          handle=".handle"
+          defaultPosition={{ x: 0, y: 0 }}
+          position={null}
+          grid={[25, 25]}
+          onStart={this.handleStart}
+          onDrag={this.handleDrag}
+          onStop={this.handleStop}
+        >
+          <div className="box">I can be dragged anywhere</div>
+        </Draggable>
+      </Chunk>
+    );
   }
 }
 
